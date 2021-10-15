e11af146216102eae1cf69d7fe3ce7be81552c97
Julie Heckler <july.heckler@gmail.com>
2021-10-15 18:48:48 +0200

    all err on submit & parse in & accents on regex

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -35,7 +35,7 @@ const textFirstname = document.getElementById("first");
 const firstnameInvalid = document.querySelector(".validation_firstname");
 
 //listening to a change from the field firstname
-textFirstname.addEventListener("change", function() {
+textFirstname.addEventListener("input", function() {
   validateFirstname(this);
 });
 
@@ -113,18 +113,25 @@ const textBirthdates = document.getElementById("birthdate");
 //getting field invalid text
 const birthdateInvalid = document.querySelector(".validation_birthdate");
 
-textBirthdates.addEventListener("change", function(e){
-  let birthYear = e.target.value.split("-")[0];
+textBirthdates.addEventListener("change", function(){
+  validateBirth(this)
+});
+
+function validateBirth (textBirthdates){
+  let birthValue = textBirthdates.value;
+  let birthYear = birthValue.split("-")[0];
   let now = new Date();
   let nowYear = now.getFullYear();
   let diffYear = Math.abs(nowYear - birthYear);
 if (diffYear <= 120){
+  birthdateInvalid.innerHTML = "";
   return true;
 }else{
   birthdateInvalid.innerHTML = "L'année n'est pas valide.";
-  return false
-  };
-})
+  birthdateInvalid.style.color = "red";
+  return false;
+  }
+};
 
 //********* validation number of competitions ************
 //getting field nombre de concours
@@ -147,67 +154,140 @@ function validateCompetitions(inputconcours) {
     return false;
   }else{
     validateRadioChecked();
+    return true;
   }
 };
     //********* validation bouton radio ************
+    //getting field location => Array locations
     const locations = document.querySelectorAll(".location");
+    //getting field invalid text
     const validCheckVille = document.querySelector(".validation_checkbox_ville");
 
     function validateRadioChecked(inputradio){
+      //transform the string in an integer
       let resConcours = parseInt(textConcours.value, 10);
+      //resConcours is Not A Number
       if (Number.isNaN(resConcours)) {
         concoursInvalid.innerHTML = "";
         validCheckVille.innerHTML = "";
         return NaN;
+
       }else{
-        console.log("pas un NaN");
+        if(resConcours === 0){ //resConcours = 0 so no need to check a radio
+          concoursInvalid.innerHTML = "";
+          validCheckVille.innerHTML = "";
+          return true;
+        }else{
+          validCheckVille.innerHTML = "Veuillez sélectionner une ville";
+          validCheckVille.style.color = "red";
           //listening to a change from the radio button
           for(let i = 0; i < locations.length; i++){
-            location[i].addEventListener("click", function() {
-              if(location[i].checked === true){
+              if(locations[i].checked === true){
                 validCheckVille.innerHTML = "";
                 return true;
               }else{
-                validCheckVille.innerHTML = "Veuillez sélectionner une ville";
-                return false;}
-            });
+                for(let i = 0; i < locations.length; i++){
+                  locations[i].addEventListener("input", function() {
+                    if(locations[i].checked === true){
+                      validCheckVille.innerHTML = "";
+                      return true;
+                    }else{
+                      return false;
+                    };
+                  });
+                };
+              };        
           };
+        };
       }
     };
-//********* validation terms of use ************
-const conditions = document.getElementById("checkbox1");
-const validConditions = document.querySelector(".valid_conditions");
 
-//Ecoute d'une sélection du bouton des conditions d'utilisation
+//********* validation terms of use ************
+//getting field checkbox1
+const conditions = document.getElementById("checkbox1");
+//getting field invalid text
+const validConditions = document.querySelector(".validation_conditions");
+
+//listening to a change from the button conditions d'utilisation
 conditions.addEventListener("click", function(){
   selectConditions(this);
 });
 
 function selectConditions(inputconditions){
   if(conditions.checked === true){
-  validConditions.innerHTML = "";
-  return true;
+    validConditions.innerHTML = "";
+    return true;
   }else{
+    validConditions.innerHTML = "Veuillez sélectionner les conditions d'utilisation";
+    validConditions.style.color = "red";
     return false;
   }
 };
 
 //********* validation formulaire ************
 const formSubmit = document.querySelector(".btn-submit");
+const form = document.querySelector("form");
+const validText = document.querySelector(".submit_text");
+//getting field to close the validation submission form
+const submitClose = document.querySelector(".close.submited");
 
-formSubmit.addEventListener("submit", function(){
+// close validation submission form
+submitClose.addEventListener("click", closeSubmission); 
+  
+function closeSubmission() {
+  validText.style.display = "none";
+};
+
+formSubmit.addEventListener("submit", function(e){
   e.preventDefault();
   validate(this);
 });
 
 function validate(e){
-  if((validateFirstname (textFirstname)) && (validateLastname (textLastname)) && (validateEmail (textEmail)) && (validateCompetitions (textConcours)) && (validateRadioChecked (locations)) && (selecConditions (conditions)))
+  if((validateFirstname (textFirstname)) && (validateLastname (textLastname)) && (validateEmail (textEmail)) && (validateCompetitions (textConcours)) && (validateBirth (textBirthdates)) && (validateRadioChecked (locations)) && (selectConditions (conditions)))
   { form.submit();
-      const validText = document.querySelector(".valid_soumission");
-      validText.style.display = "block";
-      validText.innerHTML = "Merci, votre réservation a été reçue!";
-      validText.style.color = "green";
-    }else{
+    return true;
+  }else{
+      if(validateFirstname (textFirstname) === false){
+        firstnameInvalid.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du firstname.";
+        firstnameInvalid.style.color = "red";
+      };
+      if(validateLastname (textLastname) === false){
+        lastnameInvalid.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+        lastnameInvalid.style.color = "red";
+      };
+      if(validateEmail (textEmail) === false){
+        emailInvalid.innerHTML = "L'adresse électronique est invalide.";
+        emailInvalid.style.color = "red";
+      };
+      if(validateBirth (textBirthdates) === false){
+        birthdateInvalid.innerHTML = "L'année n'est pas valide.";
+        birthdateInvalid.style.color = "red";
+      };
+      if(validateCompetitions (textConcours) === false){
+        concoursInvalid.innerHTML = "Une valeur numérique doit être saisie.";
+        concoursInvalid.style.color = "red";
+      };
+      if(validateRadioChecked (locations) === false){
+      for(let i = 0; i < locations.length; i++){
+        locations[i].addEventListener("input", function(){
+          if(locations[i].checked === false){
+            validCheckVille.innerHTML = "Veuillez sélectionner une ville";
+            validCheckVille.style.color = "red";
+          }
+        });
+      };
+      };
+      if(selectConditions (conditions) === false){
+        validConditions.innerHTML = "Veuillez sélectionner les conditions d'utilisation";
+        validConditions.style.color = "red";
+      };
       return false;
     }
+};
+
+function validateSubmit(){
+  if(validate(form) === true){
+    validText.style.display = "block";
+  };
 };
